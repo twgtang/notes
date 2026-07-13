ab54648bec687dd97e6ef3aabb0b689308959afa
twgtang <47523751+twgtang@users.noreply.github.com>
2026-07-13 16:22:30 +0800

    Add passthrough copy for prototype HTML folder

--- .eleventy.js
+++ .eleventy.js
@@ -720,6 +720,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy({ "src/site/logo.*": "/" });
+  eleventyConfig.addPassthroughCopy("src/site/prototype");
   eleventyConfig.on("eleventy.before", () => {
     normalizeFavicon(FAVICON_SOURCE, FAVICON_NORMALIZED);
   });
